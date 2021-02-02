f1b14e1707a88f88a68a1a49275a9b8907b50745
Anatoliy Kozlov <toliyansky@gmail.com>
2021-02-02 03:24:05 +0300

    Main class revert to Java for generate javadoc. Add groovy doc.

---
+++ src/main/groovy/dev/toliyansky/eval/EvaluatorSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package dev.toliyansky.eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluatorSpringBootStarterApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(EvaluatorSpringBootStarterApplication.class, args);
+    }
+
+}
